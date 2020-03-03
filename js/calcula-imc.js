9e0cee264c0c6ce7306d8a7fc9d57d398334841f
Olívia Resende <olivinharesende@hotmail.com>
2020-03-03 20:57:38 -0300

    Projeto JavaScript

---
+++ js/calcula-imc.js
@@ -0,0 +1,61 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
+
+    if(!pesoEhValido){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(!alturaEhValida){
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(alturaEhValida && pesoEhValido){
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso/(altura*altura);
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <3){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
